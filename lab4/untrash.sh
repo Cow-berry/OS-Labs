6ddb72e4494b4ee0bcb88b1856f71b406b3c1e65
Anatoly <tolya.cow@gmail.com>
2021-11-25 18:18:23 +0300

    lab4.2 complete

--- lab4/untrash.sh
+++ lab4/untrash.sh
@@ -4,13 +4,31 @@ cat .log | awk '{print substr($0, index($0, $2), length($0) - index($0, $2) + 1)
 mappings=`paste .log .names | awk -F '\t' -v name=$1 '{if ($2 == name) {print $1}}'`
 for mapping in $mappings
 do
-    NUM=`echo $mapping | awk '{print $1}'`
-    FILE=`echo $mapping | awk '{$1="";print}' | xargs`
-    echo -e -n "Do you want to restore" $FILE "\ny/n: "
-    read LINE
-    case $LINE in
+    num=`echo $mapping | awk '{print $1}'`
+    file=`echo $mapping | awk '{$1="";print}' | xargs`
+    echo -e -n "Do you want to restore" $file "\ny/n: "
+    read line
+    case $line in
         y)
-            mv ~/.trash/$NUM $FILE
+            directory=`dirname "$file"`
+            test -d $directory ||
+                {
+                echo "Can't find original folder, restoring to home"
+                file=~/"$1"
+            }
+            test -f "$file" &&
+                {
+                while true
+                do
+                    read name
+                    file=`dirname "$file"`"/$name"
+                    test -f "$file" || break
+                done
+            }
+            mv ~/.trash/$num "$file" && 
+                {
+                    cat ~/trash.log | awk -v num=$num '{if ($1 != num) {print}}' > ~/trash.log
+            }
             exit 0
             ;;
         *)
