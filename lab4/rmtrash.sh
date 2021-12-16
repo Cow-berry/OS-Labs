cbfc21a6cf7dda5a846e134484256c8e0638f185
Anatoly <tolya.cow@gmail.com>
2021-12-16 19:11:58 +0300

    lab4 fiix1

--- lab4/rmtrash.sh
+++ lab4/rmtrash.sh
@@ -1,5 +1,6 @@
 test -d ~/.trash || mkdir ~/.trash
 test -f "$1" || echo "there is no file named '$1' in current directory"
 NAME=$(echo `ls ~/.trash/` | awk '{print $NF + 1}')
-mv "`pwd`/$1" ~/.trash/$NAME
+ln "`pwd`/$1" ~/.trash/$NAME
+rm "`pwd`/$1"
 echo $NAME `pwd`/"$1" >> ~/trash.log
--- lab4/untrash.sh
+++ lab4/untrash.sh
@@ -5,30 +5,29 @@ mappings=`paste .log .names | awk -F '\t' -v name=$1 '{if ($2 == name) {print $1
 for mapping in $mappings
 do
     num=`echo $mapping | awk '{print $1}'`
-    file=`echo $mapping | awk '{$1="";print}' | xargs`
+    file=`echo $mapping | awk '{$1="";print}'`
+    file=${file:1}
+    test -f ~/.trash/$num || continue
     echo -e -n "Do you want to restore" $file "\ny/n: "
     read line
     case $line in
         y)
             directory=`dirname "$file"`
-            test -d $directory ||
-                {
+            echo $directory
+            test -d $directory || {
                 echo "Can't find original folder, restoring to home"
                 file=~/"$1"
             }
-            test -f "$file" &&
-                {
+            test -f "$file" && {
                 while true
                 do
+                    echo "file with this name already exists. Enter a new name to continue the process"
                     read name
                     file=`dirname "$file"`"/$name"
                     test -f "$file" || break
                 done
             }
-            mv ~/.trash/$num "$file" && 
-                {
-                    cat ~/trash.log | awk -v num=$num '{if ($1 != num) {print}}' > ~/trash.log
-            }
+            mv ~/.trash/$num "$file"
             exit 0
             ;;
         *)
